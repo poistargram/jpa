11470d78dd8d850307141413667f5272c8894f04
poistargram <poi.stargram@gmail.com>
2024-10-19 14:13:08 +0900

    first commit

---
+++ src/test/java/stargram/poi/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package stargram.poi.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
